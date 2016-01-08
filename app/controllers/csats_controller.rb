93b86996b17a72ffddc647e593d112a4e3aabf3f
Abhishek Sarkar <abhisheksarka@gmail.com>
2016-01-08 16:03:17 +0530

    configuration of csat module added

--- app/controllers/csats_controller.rb
+++ app/controllers/csats_controller.rb
@@ -1,5 +1,13 @@
 class CsatsController < ApplicationController
+  before_filter :load_project, :authorize, only: :index
+
   def index
     
+  end
+
+  private
+
+  def load_project
+    @project = Project.find(params[:project_id])
   end
 end
--- init.rb
+++ init.rb
@@ -5,4 +5,9 @@ Redmine::Plugin.register :catalyst_csat do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+  project_module :csats do
+    permission :view_csats, csats: :index
+  end
+  menu :project_menu, :CSATs, { controller: 'csats', action: 'index' }, caption: 'CSAT Report', param: :project_id
 end
