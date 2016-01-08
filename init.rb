5824fccb928810d823674af63211ee52043440af
Abhishek Sarkar <abhisheksarka@gmail.com>
2016-01-08 15:24:26 +0530

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :catalyst_csat do
+  name 'Catalyst CSAT plugin'
+  author 'Abhishek Sarkar'
+  description 'Automates the client feedback flow.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
