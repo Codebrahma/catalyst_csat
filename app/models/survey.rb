20511d5d38394a80989b8886111757b03c4cb17d
Abhishek Sarkar <abhisheksarka@gmail.com>
2016-01-11 17:36:22 +0530

    fixed validation issue while form submission

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,8 +1,8 @@
 class Survey < ActiveRecord::Base
   unloadable
 
-  validates :delivered_as_promised, presence: true, inclusion: { in: %w(1 2 3).map(&:to_i) }
-  validates :delivered_on_time, presence: true, inclusion: { in: %w(1 2 3).map(&:to_i) }
+  validates :delivered_as_promised, presence: true, inclusion: { in: %w(0 1).map(&:to_i) }
+  validates :delivered_on_time, presence: true, inclusion: { in: %w(0 1).map(&:to_i) }
   validates :satisfaction, presence: true, inclusion: { in: %w(1 2 3).map(&:to_i) }
 
 end
